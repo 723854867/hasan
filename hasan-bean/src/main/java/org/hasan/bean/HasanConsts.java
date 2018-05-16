4e1ad517f4b5bcc2e61ac5abb9a6726ba1ea0305
lynn <723854867@qq.com>
2018-05-16 14:16:38 +0800

    lynn

--- hasan-bean/src/main/java/org/hasan/bean/HasanConsts.java
+++ hasan-bean/src/main/java/org/hasan/bean/HasanConsts.java
@@ -9,13 +9,13 @@ import org.gatlin.core.bean.model.option.StrOption;
 public interface HasanConsts {
 
 	// 默认会员名称
-	final StrOption DEFAULT_MEMBER_TITLE					= new StrOption("default.member.title", "普通会员");
+	final StrOption DEFAULT_MEMBER_TITLE					= new StrOption("default_member_title", "普通会员");
 	// 订单打包最小时间间隔
-	final IntegerOption ORDER_ADVANCE_MINUTES				= new IntegerOption("order.advance.minutes", 30);
+	final IntegerOption ORDER_ADVANCE_MINUTES				= new IntegerOption("order_advance_minutes", 30);
 	// 默认显示评价数
-	final IntegerOption DEFAULT_EVALUATION_NUM				= new IntegerOption("default.evaluation.num", 3);
+	final IntegerOption DEFAULT_EVALUATION_NUM				= new IntegerOption("default_evaluation_num", 3);
 	// 默认快递费
-	final ObjectOption<BigDecimal> EXPRESS_FEE				= new ObjectOption<BigDecimal>("express.fee", BigDecimal.valueOf(15));
+	final ObjectOption<BigDecimal> EXPRESS_FEE				= new ObjectOption<BigDecimal>("express_fee", BigDecimal.valueOf(15));
 	// 配送可预约时间最大显示数
 	final IntegerOption DELIVERY_TIME_MAXIMUM				= new IntegerOption("delivery_time_maximum", 3);
 }
