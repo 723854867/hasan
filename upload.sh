8fe466b5e0ca478fc28a7b822f734343d2df2c8c
lynn <723854867@qq.com>
2018-07-04 11:46:58 +0800

    lynn

--- upload.sh
+++ upload.sh
@@ -2,5 +2,9 @@
 cd `dirname $0`
 BASE_DIR=`pwd`
 echo "Starting upload hasan.war"
-scp $BASE_DIR/hasan-web/target/hasan.war root@121.196.193.96:/root/web/hasan/webapps
+DIR="hasan_test"
+if [ "$1" = "online" ] ; then
+	DIR="hasan_online"
+fi
+scp $BASE_DIR/hasan-web/target/hasan.war root@121.196.193.96:/root/web/$DIR/webapps
 
